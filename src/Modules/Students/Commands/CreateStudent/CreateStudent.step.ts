e68ef3345d1d73cc6e435730698551df7935aad4
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-02-04 21:50:24 -0600

    Feature: Student names must now be all letters

--- src/Modules/Students/Commands/CreateStudent/CreateStudent.step.ts
+++ src/Modules/Students/Commands/CreateStudent/CreateStudent.step.ts
@@ -1,22 +1,27 @@
 import { Given, Then, When } from '@cucumber/cucumber';
 import { CreateStudentCommand } from './CreateStudentCommand';
-import { CreateStudentRequest } from './CreateStudentRequest';
 import { expect } from 'chai';
+import { UsersFacade } from '../../../Authentication/Services/UsersFacade';
+import { InMemoryUserRepository } from '../../../Authentication/Repositories/UserRepository/InMemoryUserRepository';
 
-const useCase = new CreateStudentCommand();
-let request: CreateStudentRequest = { name: '' };
+const userRepo = new InMemoryUserRepository();
+const fakeUserFacade = new UsersFacade(userRepo);
+const useCase = new CreateStudentCommand(fakeUserFacade);
+
 let response: boolean = false;
 
 Given('I am a Teacher', function () {
 });
 
 When(/I go to register a new Student named (.*)/, function (name: string) {
-    request.name = name;
-
-    response = useCase.handle(request);
+    response = useCase.handle({ name });
 });
 
 Then(/I should see (.*) in the Student List/, function (name: string) {
     expect(response).to.equal(true);
+});
+
+Then(/I should NOT see (.*) in the Student List/, function (name: string) {
+    expect(response).to.equal(false);
 });
 
--- src/Modules/Students/Commands/CreateStudent/CreateStudentCommand.ts
+++ src/Modules/Students/Commands/CreateStudent/CreateStudentCommand.ts
@@ -1,8 +1,17 @@
 import { CommandHandler } from '../../../../Shared/Application/Command/CommandHandler';
 import { CreateStudentRequest } from './CreateStudentRequest';
+import { Users } from '../../../Authentication/Contracts/Users';
 
 export class CreateStudentCommand implements CommandHandler<CreateStudentRequest, boolean> {
+    constructor(private readonly userService: Users) {
+    }
+
     handle(request: CreateStudentRequest): boolean {
-        return true;
+        const validNameRegex = /^[a-zA-Z]+$/;
+        if (!validNameRegex.test(request.name)) {
+            return false;
+        }
+
+        return this.userService.registerUser(request.name);
     }
 }
