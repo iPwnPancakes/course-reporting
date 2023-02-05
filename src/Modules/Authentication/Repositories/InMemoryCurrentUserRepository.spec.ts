f126b9edef31475c0064893262978f6e5c2eeab5
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-02-04 18:40:36 -0600

    Tested getting and setting current user

--- src/Modules/Authentication/Repositories/InMemoryCurrentUserRepository.spec.ts
+++ src/Modules/Authentication/Repositories/InMemoryCurrentUserRepository.spec.ts
@@ -1,5 +1,6 @@
 import { describe } from 'mocha';
 import { InMemoryCurrentUserRepository } from './InMemoryCurrentUserRepository';
+import { expect } from 'chai';
 
 describe('InMemoryCurrentUserRepository', () => {
     let repo: InMemoryCurrentUserRepository;
@@ -11,6 +12,13 @@ describe('InMemoryCurrentUserRepository', () => {
     describe('setting current user', () => {
         it('accepts "Daniel"', () => {
             repo.setCurrentUser('Daniel');
+        });
+    });
+
+    describe('getting current user', () => {
+        it('gets current user after its been set', () => {
+            repo.setCurrentUser('Daniel');
+            expect(repo.getCurrentUser()).to.equal('Daniel');
         });
     });
 });
--- src/Modules/Authentication/Repositories/InMemoryCurrentUserRepository.ts
+++ src/Modules/Authentication/Repositories/InMemoryCurrentUserRepository.ts
@@ -1,6 +1,11 @@
 export class InMemoryCurrentUserRepository {
+    private currentUser: string | null = null;
 
     setCurrentUser(user: string) {
+        this.currentUser = user;
+    }
 
+    getCurrentUser(): string | null {
+        return this.currentUser;
     }
 }
