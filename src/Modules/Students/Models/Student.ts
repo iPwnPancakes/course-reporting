211e597b5e205fcfeae33dd4234a21b408b79dd0
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-02-07 23:18:11 -0600

    Student now uses validation logic from Name model

--- src/Modules/Students/Models/Student.ts
+++ src/Modules/Students/Models/Student.ts
@@ -1,20 +1,21 @@
 import { Result } from '../../../Shared/Application/Result/Result';
 import { ValidationError } from '../../../Shared/Application/Errors/ValidationError';
 import { StudentEmail } from './StudentEmail';
+import { Name } from './Name';
 
 export class Student {
     private constructor(private readonly name: string, private email: StudentEmail) {
     }
 
     static make(name: string, email: string): Result<Student, ValidationError> {
-        const validNameRegex = /^[a-zA-Z]+$/;
-        if (!validNameRegex.test(name)) {
-            return { ok: false, error: new ValidationError('Name contained invalid characters') };
+        const nameOrError = Name.make(name);
+        if (nameOrError.ok === false) {
+            return nameOrError;
         }
 
         const studentEmailOrError = StudentEmail.make(email);
-        if (!studentEmailOrError.ok) {
-            return studentEmailOrError as { ok: false, error: ValidationError };
+        if (studentEmailOrError.ok === false) {
+            return studentEmailOrError;
         }
 
         return { ok: true, value: new Student(name, studentEmailOrError.value) };
