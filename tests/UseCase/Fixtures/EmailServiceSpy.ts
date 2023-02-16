758e2e9a1bc8df0930e8f4b654d57240e28b9e1c
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-02-16 15:35:23 -0600

    Deleted unused manual EmailServiceSpy

--- tests/UseCase/Fixtures/EmailServiceSpy.ts
+++
@@ -1,9 +0,0 @@
-import { IEmailService } from "../../../src/Modules/Email/Contracts/IEmailService";
-
-export class EmailServiceSpy implements IEmailService {
-    public newStudentRegistrationEmailCalls = 0;
-
-    sendNewStudentRegistrationEmail(studentName: string, emails: string[]) {
-        this.newStudentRegistrationEmailCalls++;
-    }
-}
